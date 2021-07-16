c346c5ac2ccbc2610f7789d613972d1d987d3900
LAPTOP-O1LFPQB2\48505 <marcin.bala@itrun.pl>
2021-07-16 08:15:40 +0200

    readme and queries update

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # Identity IQ JDBC project
 
 ### 1. Project description
-Project provides connection with Identity IQ SQL Server database and allows performing queries. In order to run query use main method in Queries class. Query can be changed by providing different agrumnents. Currently there are already two prepared arguments for active managers and application without owner.
+Project provides connection with Identity IQ SQL Server database and allows performing SQL queries. Query can be changed by providing different arguments. 
+
+### 2. Project methods
+Methods for performing queries are stored in Queries class:
+1. activeManagerQuery - for active managers.
+2. applicationWithoutOwnerQuery - application without owner.
+
+### 3. Demo
+In order to run query use main method in Queries class.
+
+### 4. Requirements
+Technologies used in project:
+```bash
+1) Java 8
+2) Gradle 6.8
+```
+
+### 5. Troubleshooting
+If you encounter any problems regarding operation, please let me know. 
+
--- src/main/java/com/creativelabs/identityiq/jdbc/Queries.java
+++ src/main/java/com/creativelabs/identityiq/jdbc/Queries.java
@@ -4,9 +4,8 @@ import java.sql.*;
 
 public class Queries {
 
-    public void performQuery(String sqlQuery) {
+    public void activeManagerQuery(String sqlQuery) {
         Connection conn = null;
-
         try {
 
             String dbURL = "jdbc:sqlserver://localhost:1433;databaseName=identityiq";
@@ -39,11 +38,47 @@ public class Queries {
         }
     }
 
+    public void applicationWithoutOwnerQuery(String sqlQuery) {
+        Connection conn = null;
+        try {
+
+            String dbURL = "jdbc:sqlserver://localhost:1433;databaseName=identityiq";
+            String user = "sa";
+            String pass = "march";
+            conn = DriverManager.getConnection(dbURL, user, pass);
+            if (conn != null) {
+                DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
+                Statement statement = DriverManager.getConnection(dbURL, user, pass).createStatement();
+                ResultSet rs = statement.executeQuery(sqlQuery);
+
+                while(rs.next()) {
+                    System.out.println(rs.getString("NAME"));
+                }
+                rs.close();
+                statement.close();
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Queries queries = new Queries();
-        String activeManagerQuery = "SELECT [firstname],[lastname],[manager_status] FROM [identityiq].[identityiq].[spt_identity] " +
+        String activeManager = "SELECT [firstname],[lastname],[manager_status] FROM [identityiq].[identityiq].[spt_identity] " +
                 "where manager_status = 1 and inactive = 0";
         String applicationWithoutOwner = "SELECT [name] FROM [identityiq].[identityiq].[spt_application] where owner is NULL";
-        queries.performQuery(activeManagerQuery);
+        System.out.println("Active managers:");
+        queries.activeManagerQuery(activeManager);
+        System.out.println("Application without owner:");
+        queries.applicationWithoutOwnerQuery(applicationWithoutOwner);
     }
 }
